707a2b72d2483f62a122a952f917c3c6cb05ba79
Chunxiao Li <ianli@gra-login2.graham.sharcnet>
2019-11-09 21:58:12 -0500

    modified path

--- run.sh
+++ run.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
+#./run.sh path_to_directory file_name full_path_to_treewidth_solver
+
 path=$1
 file=$2
+twsolver=$3
 
-/home/ianli/CDCL-proof-structural-analysis/executables/glucose $path/$file -certified -certified-output=/scratch/ianli/DRUPproof/$file.drup > /scratch/ianli/glucoseResult/$file.glucose
+echo
+echo solving
+echo
 
-/home/ianli/CDCL-proof-structural-analysis/executables/drat-trim $path/$file /scratch/ianli/DRUPproof/$file.drup -l /scratch/ianli/DRATcore/$file.core.drat
+~/CDCL-proof-structural-analysis/executables/glucose $path/$file -certified -certified-output=~/scratch/DRUPproof/$file.drup > ~/scratch/glucoseResult/$file.glucose
 
-/home/ianli/CDCL-proof-structural-analysis/executables/drat-trim $path/$file /scratch/ianli/DRATcore/$file.core.drat -r /scratch/ianli/coreDependency/$file.dependency
+echo
+echo computing DRAT core
+echo
 
-python /home/ianli/CDCL-proof-structural-analysis/scripts/dependencyToGR.py /scratch/ianli/coreDependency/ $file.dependency
+~/CDCL-proof-structural-analysis/executables/drat-trim $path/$file ~/scratch/DRUPproof/$file.drup -l ~/scratch/DRATcore/$file.core.drat
 
+echo
+echo computing dependency of DRAT \in TraceCheck format
+echo
 
+~/CDCL-proof-structural-analysis/executables/drat-trim $path/$file ~/scratch/DRATcore/$file.core.drat -r /scratch/ianli/coreDependency/$file.dependency
+
+echo
+echo converting TraceCheck to .gr format
+echo
+
+python ~/CDCL-proof-structural-analysis/scripts/dependencyToGR.py ~/scratch/coreDependency/ $file.dependency
+
+echo
+echo computing tree decomposition
+echo
+
+$twsolver ~/scratch/coreGR/$file.dependency.gr > ~/scratch/treeDecomposition/$file.decomposition
