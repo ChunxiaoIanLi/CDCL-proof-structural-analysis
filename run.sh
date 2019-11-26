5b7c16441f1a5936d8e989c045b21de37fe4e56f
Illya Myshakov <imyshakov@gmail.com>
2019-11-25 23:10:25 -0500

    Typo fix

--- run.sh
+++ run.sh
@@ -20,13 +20,13 @@ echo
 echo solving
 echo
 
-start=$(($(date +%s%N)/1000000)
+start=$(($(date +%s%N)/1000000))
 
 ~/CDCL-proof-structural-analysis/executables/glucose $path/$file -certified -certified-output=~/scratch/DRUPproof/$file.drup > ~/scratch/glucoseResult/$file.glucose
 
-end=$(($(date +%s%N)/1000000)
+end=$(($(date +%s%N)/1000000))
 
-diff=$(($end-$start)
+diff=$(($end-$start))
 
 echo
 echo "Glucose executed in:" + $diff + "milliseconds"
@@ -37,13 +37,13 @@ echo
 echo computing DRAT core
 echo
 
-start=$(($(date +%s%N)/1000000)
+start=$(($(date +%s%N)/1000000))
 
 ~/CDCL-proof-structural-analysis/executables/drat-trim $path/$file ~/scratch/DRUPproof/$file.drup -l ~/scratch/DRATcore/$file.core.drat
 
-end=$(($(date +%s%N)/1000000)
+end=$(($(date +%s%N)/1000000))
 
-diff=$(($end-$start)
+diff=$(($end-$start))
 
 echo
 echo "Core computation executed in:" + $diff + "milliseconds"
@@ -54,13 +54,13 @@ echo
 echo computing dependency of DRAT \in TraceCheck format
 echo
 
-start=$(($(date +%s%N)/1000000)
+start=$(($(date +%s%N)/1000000))
 
 ~/CDCL-proof-structural-analysis/executables/drat-trim $path/$file ~/scratch/DRATcore/$file.core.drat -r /scratch/ianli/coreDependency/$file.dependency
 
-end=$(($(date +%s%N)/1000000)
+end=$(($(date +%s%N)/1000000))
 
-diff=$(($end-$start)
+diff=$(($end-$start))
 
 echo
 echo "Dependency computation executed in:" + $diff + "milliseconds"
@@ -71,13 +71,13 @@ echo
 echo converting TraceCheck to .gr format
 echo
 
-start=$(($(date +%s%N)/1000000)
+start=$(($(date +%s%N)/1000000))
 
 python ~/CDCL-proof-structural-analysis/scripts/dependencyToGR.py ~/scratch/coreDependency/ $file.dependency
 
-end=$(($(date +%s%N)/1000000)
+end=$(($(date +%s%N)/1000000))
 
-diff=$(($end-$start)
+diff=$(($end-$start))
 
 echo
 echo ".gr conversion executed in:" + $diff + "milliseconds"
