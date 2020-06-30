c0d275214c255f4fb3ee8900e6f8186c83416260
Jonathan Chung <jt2chung@uwaterloo.ca>
2020-06-30 12:01:46 -0400

    optimized implementation of resolvability calculation

--- scripts/resolvability/main.cpp
+++ scripts/resolvability/main.cpp
@@ -78,20 +78,25 @@ static void computeCVR(double& cvr, int numClauses, int numVars) {
 	cvr = numClauses / static_cast<double>(numVars);
 }
 
+// Resolvability computation
+// O(m^2 n log(n))
 static int computeResolvable(int& numResolvable, int& numMergeable, const std::vector<std::vector<int>>& clauses) {
 	for (unsigned int i = 0; i < clauses.size(); ++i) {
 		// Initialize set for checking resolvability
+		// O(n log(n))
 		std::set<int> found;
 		for (unsigned int c_i = 0; c_i < clauses[i].size(); ++c_i) {
 			found.insert(clauses[i][c_i]);
 		}
 
 		// Compare every clause with every other clause
+		// O(m n log(n))
 		for (unsigned int j = i + 1; j < clauses.size(); ++j) {
 			bool resolvable = false;
 			long tmpNumMergeable = 0;
 			
 			// Check for resolvable/mergeable clauses
+			// O(n log(n))
 			for (unsigned int c_j = 0; c_j < clauses[j].size(); ++c_j) {
 				if (found.find(-clauses[j][c_j]) != found.end()) {
 					if (resolvable) {
@@ -105,9 +110,96 @@ static int computeResolvable(int& numResolvable, int& numMergeable, const std::v
 			}
 
 			// Update counts
+			// O(1)
 			if (resolvable) {
 				numResolvable += 1;
 				numMergeable  += tmpNumMergeable;
+			}
+		}
+	}
+
+	return 0;
+}
+
+// Optimization of resolvability computation
+// Still O(m^2 n log(n)), but faster than computeResolvable
+static int computeResolvable2(int& numResolvable, int& numMergeable, std::vector<std::vector<int>>& clauses) {
+	const auto variableComparator = [](int a, int b) {
+		return std::abs(a) < std::abs(b);
+	};
+	const auto clauseComparator = [](const std::vector<int>& a, const std::vector<int>& b) {
+		return std::abs(a[0]) < std::abs(b[0]);
+	};
+
+	// Sort variables
+	// O(m n log(n))
+	for (std::vector<int>& clause : clauses) {
+		std::sort(clause.begin(), clause.end(), variableComparator);
+	}
+
+	// Sort clauses
+	// O(m log(m))
+	std::sort(clauses.begin(), clauses.end(), clauseComparator);
+
+	// Calculate resolvability and mergeability
+	// O(m^2 n log(n))
+	// Iterate through each variable of each clause
+	for (unsigned int i = 0; i < clauses.size(); ++i) {
+		// Initialize set for checking resolvability
+		// O(n log(n))
+		std::set<int> found;
+		for (unsigned int c_i = 0; c_i < clauses[i].size(); ++c_i) {
+			found.insert(clauses[i][c_i]);
+		}
+
+		// For each variable in the clause, check for other clauses which resolve on the variable
+		for (unsigned int c_i = 0; c_i < clauses[i].size(); ++c_i) {
+			// Find the last possibly resolvable clause ahead of the current clause
+			// Since clauses are sorted, the first clause which starts with a value larger than the target variable is an upper bound
+			// on the search range.
+			// O(n + log(m))
+			const std::vector<int> toFind = { clauses[i][c_i] }; // Wrap the target variable in a clause for comparison
+			const auto resolvableUpperBound = std::upper_bound(clauses.begin() + i, clauses.end(), toFind, clauseComparator);
+
+			// Find all resolvable clauses in the range of candidates
+			// O(m n log(n))
+			for (auto candidateClauseItr = clauses.begin(); candidateClauseItr != resolvableUpperBound; ++candidateClauseItr) {
+				const std::vector<int>& candidateClause = *candidateClauseItr;				
+
+				// Check if the desired variable is potentially in the candidate clause
+				// Since variables are sorted, we can skip clauses whose maximum variables are smaller than the target variable
+				// O(1)
+				if (candidateClause.back() < std::abs(clauses[i][c_i])) continue;
+
+				// Check if the target variable is in the candidate clause
+				// Since variables are sorted, the first variable less than or equal to the target variable is a lower bound
+				// O(log(n))
+				const auto lowerBound = std::lower_bound(candidateClause.begin(), candidateClause.end(), clauses[i][c_i], variableComparator);
+				if (lowerBound == candidateClause.end()) continue; // Skip clause if no such element exists
+				if (*lowerBound != -clauses[i][c_i]) continue; // Skip clause if it does not resolve on the target variable
+
+				// Check for resolvable/mergeable clauses
+				// O(n log(n))
+				bool resolvable = false;
+				int tmpNumMergeable = 0;
+				for (unsigned int c_j = 0; c_j < candidateClause.size(); ++c_j) {
+					if (found.find(-candidateClause[c_j]) != found.end()) {
+						if (resolvable) {
+							resolvable = false;
+							break;
+						}
+						resolvable = true;
+					} else if (found.find(candidateClause[c_j]) != found.end()) {
+						++tmpNumMergeable;
+					}
+				}
+
+				// Update counts
+				// O(1)
+				if (resolvable) {
+					numResolvable += 1;
+					numMergeable  += tmpNumMergeable;
+				}
 			}
 		}
 	}
@@ -190,7 +282,12 @@ int main (const int argc, const char* const * argv) {
 		// Calculate and output num resolvable and num mergeable
 		{
 			int numResolvable = 0, numMergeable = 0;
-			computeResolvable(numResolvable, numMergeable, clauses);
+			bool useNaiveAlg = false;
+			if (useNaiveAlg) {
+				computeResolvable(numResolvable, numMergeable, clauses);
+			} else {
+				computeResolvable2(numResolvable, numMergeable, clauses);
+			}
 			writeFile(inputFileStr + ".rvm", std::bind(writeResolvability, _1, numResolvable, numMergeable));
 		}
 
