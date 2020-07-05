15e005addb5fd6472668299258a3e8d362c91c11
Chunxiao Li <ianli@gra-login1.graham.sharcnet>
2020-07-04 21:05:32 -0400

    new random formula generator

--- scripts/generate_random_degree_distribution.py
+++ scripts/generate_random_degree_distribution.py
@@ -8,7 +8,30 @@ m=int(sys.argv[2])
 k=int(sys.argv[3])
 
 
-def generateUniformVec(n, m, k):
+def generateConstantPowerVec(n, m, k, beta):
+    pd = []
+    sum = 0
+    for i in range(n):
+        weight = (i+1)**(beta)
+        pd.append(weight)
+        sum += weight
+
+    vec = []
+    current_total = 0
+    #generating vec from powerlaw
+    for i in range(n):
+        temp = int((pd[i]/float(sum))*m*k)
+        vec.append(temp)
+        current_total += temp
+
+    #distribute remainders randomly
+    for i in range(m*k - current_total):
+        vec[random.randint(0, n-1)]+=1
+
+    return vec
+
+
+def generateBalancedVec(n, m, k):
     ave = int(m*k/n)
     remainder= int((m*k)%n)
     vec = [ave]*n
@@ -17,8 +40,8 @@ def generateUniformVec(n, m, k):
         remainder-=1
     return vec
 
-def generatePowerlawVec(n, m, k):
-    vec=generateUniformVec(int(n/2), m, k)
+def generateUnbalancedVec(n, m, k):
+    vec=generateBalancedVec(int(n/2), m, k)
     
     for i in range(len(vec)):
         vec[i]-=1
@@ -31,7 +54,7 @@ def generatePowerlawVec(n, m, k):
 
 def generateMediumVec(n, m, k):
     #somehow vec[0] is not modified after this call?
-    vec=generatePowerlawVec(n, m, k)
+    vec=generateUnbalancedVec(n, m, k)
     max=vec[0]-2
     for i in range(n/2):
         diff=random.randint(0, max)
@@ -42,7 +65,7 @@ def generateMediumVec(n, m, k):
 def generateCummulative(vec):
     cummulative_vec=[vec[0]]
     for i in vec[1:]:
-        cummulative_vec.append(i + cummulative_vec[-1])
+        cummulative_vec.append(i + cummulative_vec[-1] + 1)
     return cummulative_vec
 
 def vecsum(vec):
@@ -51,71 +74,105 @@ def vecsum(vec):
         sum+=i
     return sum
 
-def generateRandomFormula(n, m, k, cummulative_vec, outf):
-    outstr=""
-    for clause in range(m):
-        temp_clause = []
-        for lit in range(k):
-            found = False
-            while not found:
-                r = random.randint(0, m*k)
-                for i in range(len(cummulative_vec)):
-                    if r <= cummulative_vec[i]:
-                        if i+1 not in temp_clause:
-                            temp_clause.append(i+1)
-                            found = True
-                            polarity=random.randint(0,1)
-                            if polarity == 0:
-                                outstr+="-"
-                            outstr+=str(i+1)
-                            outstr+=" "
-                            break;
+def vecToString(vec):
+    s = "c ["
+    for i in vec:
+        s+="{}, ".format(i)
+    s = s[:-2]
+    s += "]\n"
+    return s
+
+def generate_clause(m, k, cummulative_vec, variable_occur):
+    clause = []
+    for lit in range(k):
+        found = False
+        while not found:
+            r = random.randint(0, m*k)
+            for i in range(len(cummulative_vec)):
+                if r <= cummulative_vec[i]:
+                    if i+1 not in clause and (i+1)*-1 not in clause:
+                        found = True
+                        polarity=random.randint(0,1)
+                        if polarity == 0:
+                            clause.append((i+1)*-1)
+                        else:
+                            clause.append(i+1)
+                        variable_occur[i]+=1
                         break;
-        outstr+="0\n"
+                    break;
+    return clause, variable_occur
+
+
+def generateRandomFormula(n, m, k, cummulative_vec, outf):
+    variable_occur = [0]*n
+    cnf = []
+    for clause in range(m):
+        clause, variable_occur = generate_clause(m, k, cummulative_vec, variable_occur)
+        cnf.append(clause)
+
+    new_var_map=[-1]*n
+    new_var=1
+    for var in range(len(variable_occur)):
+        if variable_occur[var] > 0:
+            new_var_map[var] = new_var
+            new_var+=1
+
+    for clause in cnf:
+        for variable in range(len(clause)):
+            if clause[variable] > 0:
+                clause[variable] = new_var_map[abs(clause[variable])-1]
+            else:
+                clause[variable] = new_var_map[abs(clause[variable])-1]*-1
+        
+
+    # compute actual number of variables
+    actual_n = 0
+    for i in variable_occur:
+        if i > 0:
+            actual_n += 1
+
+    assert actual_n == new_var - 1
+
+    header = "p cnf {0} {1}\n".format(actual_n, m)
+    outstr = vecToString(variable_occur) + header
+    outf.write(outstr)
+
+    for clause in cnf:
+        outstr = ""
+        for variable in clause:
+            outstr += str(variable) + " "
+        outstr += "0\n"
         outf.write(outstr)
-        outstr=""
     return
 
-if not os.path.exists("instances"):
-    os.makedirs("instances")
-    if not os.path.exists("instances/uniform"):
-        os.makedirs("instances/uniform")
-    if not os.path.exists("instances/powerlaw"):
-        os.makedirs("instances/powerlaw")
-    if not os.path.exists("instances/medium"):
-        os.makedirs("instances/medium")   
+balancedvec=generateBalancedVec(n, m, k)
+unbalancedvec=generateUnbalancedVec(n, m, k)
+constantpowerneg3vec=generateConstantPowerVec(n, m, k, -3)
+constantpowerneg2point5vec=generateConstantPowerVec(n, m, k, -2.5)
+constantpowerneg2vec=generateConstantPowerVec(n, m, k, -2)
+constantpower2vec=generateConstantPowerVec(n, m, k, 2)
+constantpower2point5vec=generateConstantPowerVec(n, m, k, 2.5)
+constantpower3vec=generateConstantPowerVec(n, m, k, 3)
 
-
-#print(uniformvec, vecsum(uniformvec))
-#print(powerlawvec, vecsum(powerlawvec))
-#print(randomvec, vecsum(randomvec))
-
-#generating uniform
-uniformvec=generateUniformVec(n, m, k)
-cummulative_vec=generateCummulative(uniformvec)
-for i in range(200):
-    outf=open("instances/uniform/{0}.cnf".format(i),"w")
-    outf.write("p cnf {0} {1}\n".format(n, m))
-    generateRandomFormula(n, m, k, cummulative_vec, outf)
-    outf.close()
-
-# #generating powerlaw
-powerlawvec=generatePowerlawVec(n, m, k)
-cummulative_vec=generateCummulative(powerlawvec)
-for i in range(200):
-    outf=open("instances/powerlaw/{0}.cnf".format(i),"w")
-    outf.write("p cnf {0} {1}\n".format(n, m))
-    generateRandomFormula(n, m, k, cummulative_vec, outf)
-    outf.close()
-
-# #generating medium
-for i in range(200):
-    mediumvec=generateMediumVec(n, m, k)
-    cummulative_vec=generateCummulative(mediumvec)
-    outf=open("instances/medium/{0}.cnf".format(i),"w")
-    outf.write("p cnf {0} {1}\n".format(n, m))
-    generateRandomFormula(n, m, k, cummulative_vec, outf)
-    outf.close()
-
+num_of_instanes = 200
+if os.path.exists("balanced") and len(os.listdir('balanced') ) == 0:
+    cummulative_vec=generateCummulative(balancedvec)
+    for i in range(num_of_instanes):
+        outf=open("balanced/{0}.cnf".format(i),"w")
+        generateRandomFormula(n, m, k, cummulative_vec, outf)
+        outf.close()
+if os.path.exists("unbalanced") and len(os.listdir('unbalanced') ) == 0:
+    cummulative_vec=generateCummulative(unbalancedvec)
+    for i in range(num_of_instanes):
+        outf=open("unbalanced/{0}.cnf".format(i),"w")
+        generateRandomFormula(n, m, k, cummulative_vec, outf)
+        outf.close()
+if os.path.exists("medium") and len(os.listdir('medium') ) == 0:
+    for i in range(num_of_instanes):
+        mediumvec=generateMediumVec(n, m, k)
+        cummulative_vec=generateCummulative(mediumvec)
+        outf=open("medium/{0}.cnf".format(i),"w")
+        generateRandomFormula(n, m, k, cummulative_vec, outf)
+        outf.close()
 
 
