6643ca5b712bf2f13815096764a5459cd920bf5e
Jonathan Chung <jt2chung@uwaterloo.ca>
2020-08-01 20:19:04 -0400

    adding Soham's script for generating plots

---
+++ scripts/explorations.py
@@ -0,0 +1,48 @@
+#--------------------------------------------------------------------
+# Do some preliminiary explorations on the data, i.e., 
+# understand the dependence of the parameters (both set and inferred)
+# with the solving time
+# Soham 07/2020
+#--------------------------------------------------------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
+import matplotlib as mpl
+mpl.rcParams.update({
+        "font.size": 10.0,
+        "axes.titlesize": 10.0,
+        "axes.labelsize": 10.0,
+        "xtick.labelsize": 10.0,
+        "ytick.labelsize": 10.0,
+        "legend.fontsize": 10.0,
+        "figure.dpi": 300,
+        "savefig.dpi": 300,
+        "text.usetex": True
+})
+
+
+# Load data
+df = pd.read_csv("../data/statsSATSolver.csv").drop(['instance', 'SAT/UNSAT', 'clauses', 'beta', 'vars'], axis=1)
+
+# Plot correlations
+fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(20, 20))
+for i, column1 in enumerate(df.columns):
+    for j, column2 in enumerate(df.columns):
+        axes[i,j].scatter(df[column1], df[column2], c=df['time'], s=df['time']/20, alpha=0.75, edgecolors='none')
+        axes[i,j].set_xlabel("{0}".format(column1))
+        axes[i,j].set_ylabel("{0}".format(column2))
+plt.savefig("../output/scatter.pdf")
+plt.close()
+
+# Let's do a PCA
+print(df.columns)
+min_max_scaler = preprocessing.MinMaxScaler()
+pca = PCA()
+df = min_max_scaler.fit_transform(df)
+pca.fit(df)
+print(pca.components_)
+print(pca.explained_variance_)
