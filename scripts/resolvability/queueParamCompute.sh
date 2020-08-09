bd7e135e405a70e3536438635166e2d2f3eb8b1c
Jonathan Chung <jt2chung@uwaterloo.ca>
2020-08-09 10:40:35 -0400

    auto-retry if sbatch fails to queue a job

--- scripts/resolvability/queueParamCompute.sh
+++ scripts/resolvability/queueParamCompute.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+MAX_RETRIES=3;
+
 (
-	find /scratch/ianli/parameters_of_industiral/crafted/ -name "*.cnf"
-	find /scratch/ianli/parameters_of_industiral/crypto/ -name "*.cnf"
-	find /scratch/ianli/parameters_of_industiral/random/ -name "*.cnf"
-	find /scratch/ianli/parameters_of_industiral/verification/ -name "*.cnf"
+	find /scratch/ianli/parameters_of_industiral/ -name "*.cnf"
 ) | while read LINE; do
 	OUTFILE="${LINE%.cnf}.cmp.sh"
 	cp sbatchBaseFile.sh "${OUTFILE}"
 	echo "#SBATCH --output=${LINE%.cnf}.cmp.log" >> "${OUTFILE}"
-	echo "time /home/jt2chung/sha1-unsat/CDCL-proof-structural-analysis/scripts/resolvability/countResolvable ${LINE}" >> "${OUTFILE}"
+	echo "time /home/jt2chung/sha1-unsat/CDCL-proof-structural-analysis/scripts/resolvability/countResolvable -a ${LINE}" >> "${OUTFILE}"
 	chmod +x "${OUTFILE}"
 	echo "${LINE%.cnf}"
-	sbatch $OUTFILE
+	for (( i=0; i <= ${MAX_RETRIES}; ++i )); do
+		if sbatch $OUTFILE; then
+			break
+		else
+			echo "Retrying... $((i + 1))/${MAX_RETRIES}"
+		fi
+	done
 	sleep 2
 done
