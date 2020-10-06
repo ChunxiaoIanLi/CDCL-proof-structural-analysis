f9fd54188b97af736722ea23fc69a7cb22525a9d
Jonathan Chung <jt2chung@uwaterloo.ca>
2020-10-06 12:02:03 -0400

    add edge weights for self-loops

--- hierarchical_community_structure/selfLoopCommunities.py
+++ hierarchical_community_structure/selfLoopCommunities.py
@@ -1,9 +1,15 @@
 import igraph
+import math
 import sys
 from cnf_to_edge_set import cnf_to_edge_set, read_file, cnf_to_clauses_list
 import os
 import numpy as np
 from random import randrange
+
+def addWeightedLoops(edge_list, weight_list, n, weight):
+	for i in range(n):
+		edge_list.append([i, i])
+		weight_list.append(weight)
 
 def computeCommunityStructure(g):
 	vertex_clustering = g.community_multilevel()
@@ -11,8 +17,9 @@ def computeCommunityStructure(g):
 	maxCommunitySize = 0
 	numCommunities = len(vertex_clustering)
 	for community in vertex_clustering:
-		totalCommunitySize += len(community)
-		maxCommunitySize = max(maxCommunitySize, len(community))
+		communitySize = len(community)
+		totalCommunitySize += communitySize
+		maxCommunitySize = max(maxCommunitySize, communitySize)
 	avgCommunitySize = totalCommunitySize / float(numCommunities)
 	modularity = g.modularity(vertex_clustering)
 	return numCommunities, avgCommunitySize, maxCommunitySize, modularity
@@ -27,17 +34,20 @@ file = sys.argv[1]
 clauses, m, n = read_file(file)
 edge_set = cnf_to_edge_set(clauses)
 edge_list = [list(e) for e in edge_set]
+weight_list = []
 clauses_list = cnf_to_clauses_list(clauses)
 
-# Add self-loops
-for i in range(n):
-	edge_list.append([i, i])
+# Add weights for normal edges
+for e in edge_list:
+	weight_list.append(1)
+
+# Add weighted self-loops
+weight = math.sqrt(n / math.log(n))
+addWeightedLoops(edge_list, weight_list, n, weight)
 
 g = igraph.Graph()
 g.add_vertices(n)
 g.add_edges(edge_list)
-for i in range(n):
-	g.vs[i]['name'] = i
 
 numCommunities, avgCommunitySize, maxCommunitySize, modularity = computeCommunityStructure(g)
 write_data(file, numCommunities, avgCommunitySize, maxCommunitySize, modularity)
