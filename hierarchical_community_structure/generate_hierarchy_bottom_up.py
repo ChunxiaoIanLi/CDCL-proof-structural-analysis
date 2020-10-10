727f4e17cf81f5ae696697d3912484c5278aefaa
Xiao Tong Mu <xiao.mu@uwaterloo.ca>
2020-10-10 19:09:57 -0400

    Fixed and reformatted generate_hierarchy_bottom_up

--- hierarchical_community_structure/generate_hierarchy_bottom_up.py
+++ hierarchical_community_structure/generate_hierarchy_bottom_up.py
@@ -4,10 +4,12 @@ def compute_modularity(m, vwpairs, expected_edge):
 	q = (vwpairs - expected_edge * vwpairs) / (2 * m)
 	return q
 
-def generate_hierarchy_bottom_up(k, c, m, vwpairs, expected_edge, level):
-        i = 0
-        if level == 1:
-		i = k - 2
+def generate_hierarchy_bottom_up(k, c, m, vwpairs, expected_edge, level, k_minus):
+	if level == 5:
+		return
+	i = 0
+	if level == 1:
+		i = k - k_minus
 		#initial total number of edges
 		m = (c * (k**2 - k) + (c**2 - c) * i * k)/2
 		#initial number of vw pairs, where v!=w and v, w are both from the same community.
@@ -18,7 +20,7 @@ def generate_hierarchy_bottom_up(k, c, m, vwpairs, expected_edge, level):
 
 	current_q = compute_modularity(m, vwpairs, expected_edge)
 	if level == 1:
-		print("level: {0} i: {1} k:{2} i/k: {3} q: {4}".format(level, i, k, float(i)/k, current_q))
+		print("level: {0} i: {1} k: {2} i/k: {3} q: {4}".format(level, i, k, float(i)/k, current_q))
 	
 	# print(m, vwpairs, expected_edge)
 	# print(current_q)
@@ -46,13 +48,18 @@ def generate_hierarchy_bottom_up(k, c, m, vwpairs, expected_edge, level):
 
 	level += 1
 	print("level: {0} i: {1} k: {2} i/k: {3} q: {4}".format(level, i, next_k, float(i)/next_k, next_q))
-	generate_hierarchy_bottom_up(next_k, c, next_m, next_vwpairs, next_expected_edge, level)
+	generate_hierarchy_bottom_up(next_k, c, next_m, next_vwpairs, next_expected_edge, level, k_minus)
 	return
 
 #size of each community
 k = int(sys.argv[1])
 #degree of the hierarchy tree
 c = int(sys.argv[2])
+#i = k - k_minus
+if len(sys.argv) == 4:
+	k_minus = int(sys.argv[3])
+else:
+	k_minus = 2
 
 #initial total number of edges
 m = 0
@@ -64,7 +71,7 @@ level = 1
 
 
 
-generate_hierarchy_bottom_up(k, c, m, vwpairs, expected_edge, level)
+generate_hierarchy_bottom_up(k, c, m, vwpairs, expected_edge, level, k_minus)
 
 
 
