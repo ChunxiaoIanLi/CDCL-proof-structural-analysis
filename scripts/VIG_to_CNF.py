6851b212b794e3ca063290341f304fd62cd2170f
chunxiaoianli <chunxiao.ian.li@gmail.com>
2020-12-08 11:06:44 -0500

    instead of a adding three random literals (by first checking whether they form a clique) to a clause, we add a random 3-clique as a clause

--- scripts/VIG_to_CNF.py
+++ scripts/VIG_to_CNF.py
@@ -3,6 +3,7 @@ import sys
 import os
 import random
 import math
+from cnf_to_edge_set import cnf_to_edge_set, read_file, cnf_to_clauses_list
 
 def generateUniformVec(n, m, k):
 	ave = int(m*k/n)
@@ -29,6 +30,21 @@ def vecsum(vec):
 		sum+=i
 	return sum
 
+def initialize_graph(g):
+	n = g.vcount()
+	for i in range(n):
+		g.vs[i]['name'] = i
+		for e in g.es:
+			e["visited"] = False
+	return g
+
+def var_to_lit(v):
+	polarity=random.randint(0,1)
+	if polarity == 0:
+		return -1*(v+1)
+	else:
+		return v+1
+
 def pick_k_lits(dv, k):
 	# TODO: random.choices()
 	cummulative_vec = generateCummulative(dv)
@@ -39,11 +55,7 @@ def pick_k_lits(dv, k):
 		# TODO: binary search or search for python functions that do this
 		for v in range(len(cummulative_vec)):
 			if r < cummulative_vec[v]:
-				polarity=random.randint(0,1)
-				if polarity == 0:
-					k_lits.append(-1*(v+1))
-				else:
-					k_lits.append(v+1)
+				k_lits.append(var_to_lit(v))
 				break
 	return k_lits
 
@@ -51,7 +63,8 @@ def is_clique(g, k_lits):
 	vertices = []
 	#TODO this is assuming vertex 0 corresponds to variable 1, etc
 	for i in k_lits:
-		vertices.append(abs(i)-1)
+		if not abs(i)-1 in vertices:
+			vertices.append(abs(i)-1)
 
 	# TODO: perhaps use g.es.select(_within=vertices) and check the number of edges
 	subgraph = g.subgraph(vertices)
@@ -72,6 +85,13 @@ def all_edges_visited(g):
 			return False
 	return True
 
+def count_unvisited(g):
+	counter = 0
+	for e in g.es:
+		if e["visited"] == False:
+			counter+=1
+	return counter
+
 def print_cnf(cnf, n, m):
 	print("p cnf {0} {1}".format(n, m))
 	for c in cnf:
@@ -83,10 +103,75 @@ def print_cnf(cnf, n, m):
 		print(outstr)
 	return
 
+def write_cnf(cnf, n, m, file):
+	fp = open(file, "w")
+	fp.write("p cnf {0} {1}\n".format(n, m))
+	for c in cnf:
+		outstr = ""
+		for l in c:
+			outstr+=str(l)
+			outstr+=" "
+		outstr+="0\n"
+		fp.write(outstr)
+	return
+
 def reset_visited_flag(g):
 	for e in g.es:
 		e["visited"] = False
 	return
+
+def compute_phase_one_clauses(g, k):
+	clauses = []
+	for e in g.es:
+		if e["visited"] == False:
+			clause = []
+			u = e.source
+			v = e.target
+			# try to find a k-clique
+			u_neighbors = g.neighbors(u)
+			v_neighbors = g.neighbors(v)
+			common_vertices = list(set(u_neighbors).intersection(v_neighbors))
+			vertices_in_clique = []
+			vertices_in_clique.append(u)
+			vertices_in_clique.append(v)
+
+			clause.append(var_to_lit(u))
+			clause.append(var_to_lit(v))
+			if len(common_vertices) >= k - 2:
+				w = random.sample(common_vertices, 1)[0]
+				vertices_in_clique.append(w)
+				clause.append(var_to_lit(w))
+
+			edges_in_clique = g.es.select(_within=vertices_in_clique)
+			for i in edges_in_clique:
+				i['visited'] = True
+
+			clauses.append(clause)
+	return clauses
+
+def compute_phase_two_clauses(g, k, m):
+	clauses = []
+	for i in range(m):
+		e = random.sample(g.es, 1)[0]
+		clause = []
+		u = e.source
+		v = e.target
+		# try to find a k-clique
+		u_neighbors = g.neighbors(u)
+		v_neighbors = g.neighbors(v)
+		common_vertices = list(set(u_neighbors).intersection(v_neighbors))
+		vertices_in_clique = []
+		vertices_in_clique.append(u)
+		vertices_in_clique.append(v)
+
+		clause.append(var_to_lit(u))
+		clause.append(var_to_lit(v))
+		if len(common_vertices) >= k - 2:
+			w = random.sample(common_vertices, 1)[0]
+			vertices_in_clique.append(w)
+			clause.append(var_to_lit(w))
+		clauses.append(clause)	
+	return clauses
 
 # g: VIG
 # m: number of clauses to generate
@@ -96,59 +181,52 @@ def VIG_to_CNF(g, m, k):
 	# where variable occurrences are uniform
 	# degree_distribution is a vector of integers
 	dv = generateUniformVec(g.vcount(), m, k)
+	initialize_graph(g)
 
-	trial = 1
+	# cnf is a vector of clauses, a clause is a vector of integers
+	cnf = []
+	# TODO: assignVarsToDegreeDistribution()
+	# Phase 1: generate clauses such that every edge appears in some clauses
 	while True:
-		print(trial)
-		# cnf is a vector of clauses, a clause is a vector of integers
-		cnf = []
-		# TODO: assignVarsToDegreeDistribution()
-		for i in range(m):
-			# Step 1: adding the new clause to the formula
-			while True:
-				# TODO: are we allowing to generate clauses with width less than k
-				k_lits = pick_k_lits(dv, k)
-				if is_clique(g, k_lits):
-					break
-			cnf.append(k_lits)
-			# Step 2: label the edges corresponding to the added clause as "visited"
-			mark_visited(g, k_lits)
+		phase_one_clauses = compute_phase_one_clauses(g, k)
+		print("Phase 1: used {0}/{1} clauses to cover all edges.".format(len(phase_one_clauses), m))
+		if len(phase_one_clauses) <= m:
+			cnf = phase_one_clauses
+			break
+		else:
+			reset_visited_flag(g)
+	# Phase 2: assigning remaining edges
+	print("Phase 2: generating remaining clauses.")
+	phase_two_clauses = compute_phase_two_clauses(g, k, m - len(phase_one_clauses))
+	cnf += phase_two_clauses
+	return cnf
 
-		# TODO: we do this check if missing few edges in the VIG doesn't change the HCS too much
-		if all_edges_visited(g):
-			return cnf
-		reset_visited_flag(g)
-		trial+=1
+# file = str(sys.argv[1])
+# # n is the number of variables
+# # n = int(sys.argv[1])
+# # m is the number of clauses to generate
+# m = int(sys.argv[2])
+# # k is the width of clauses
+# k = int(sys.argv[3])
+# #p = float(sys.argv[4])
 
-n = int(sys.argv[1])
-m = int(sys.argv[2])
-k = int(sys.argv[3])
-p = float(sys.argv[4])
+# clauses, temp, n = read_file(file)
+# edge_set = cnf_to_edge_set(clauses)
+# edge_list = [list(e) for e in edge_set]
+# g = igraph.Graph()
+# g.add_vertices(n)
+# g.add_edges(edge_list)
 
-g = igraph.Graph.Erdos_Renyi(n, p)
-n = g.vcount()
-for i in range(n):
-	g.vs[i]['name'] = i
-	#g.vs[i]['visited'] = False
+# #g = igraph.Graph.Erdos_Renyi(n, p)
+# n = g.vcount()
+# for i in range(n):
+# 	g.vs[i]['name'] = i
+# 	#g.vs[i]['visited'] = False
+# 	for e in g.es:
+# 		e["visited"] = False
 
-for e in g.es:
-	e["visited"] = False
-
-layout = g.layout("large")
-visual_style = {}
-visual_style["vertex_size"] = 5
-visual_style["vertex_label"] = g.vs["name"]
-visual_style["layout"] = layout
-visual_style["bbox"] = (1000, 1000)
-visual_style["margin"] = 10
-
-igraph.plot(g, "randomgraph_200_0.6.svg", **visual_style)
-
-
-
-
-cnf = VIG_to_CNF(g, m, k)
-print_cnf(cnf, n, m)
+# cnf = VIG_to_CNF(g, m, k)
+# print_cnf(cnf, n, m)
 
 
 
