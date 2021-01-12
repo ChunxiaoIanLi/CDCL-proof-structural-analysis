20a5f2422d842c54e85f34a46849da38f32a7e95
Jonathan Chung <jt2chung@uwaterloo.ca>
2021-01-11 21:18:48 -0500

    make script queuing use source bin/activate

--- scripts/resolvability/queueArrayJob.sh
+++ scripts/resolvability/queueArrayJob.sh
@@ -9,7 +9,7 @@ INPUT_FILE=$1
 START_INDEX=$2
 END_INDEX=$((START_INDEX + $3))
 SCRIPT_FILE="arrayJobScript.sh"
-SCRIPT="/home/jt2chung/sha1-unsat/CDCL-proof-structural-analysis/hierarchical_community_structure/clustering_ed.py \${INSTANCE_NAME}"
+SCRIPT="python /home/jt2chung/sha1-unsat/CDCL-proof-structural-analysis/hierarchical_community_structure/clustering_ed.py \${INSTANCE_NAME}"
 #SCRIPT="/home/jt2chung/sha1-unsat/CDCL-proof-structural-analysis/scripts/resolvability/countResolvable -d \${INSTANCE_NAME}"
 #SCRIPT="/home/jt2chung/sha1-unsat/maplesat_static -cpu-lim=4950 \"\${INSTANCE_NAME}\" > \"\${INSTANCE_NAME}.log\""
 
@@ -21,6 +21,7 @@ echo "#SBATCH --mem=5G"                                                   >> "${
 echo "#SBATCH --job-name=countRes"                                        >> "${SCRIPT_FILE}"
 echo "#SBATCH --array=${START_INDEX}-${END_INDEX}"                        >> "${SCRIPT_FILE}"
 echo ""                                                                   >> "${SCRIPT_FILE}"
+echo "source /home/jt2chung/sat/bin/activate"
 echo "INSTANCE_NAME=\`sed -n \${SLURM_ARRAY_TASK_ID}p ${INPUT_FILE}\`"    >> "${SCRIPT_FILE}"
 echo "${SCRIPT}"                                                          >> "${SCRIPT_FILE}"
 
