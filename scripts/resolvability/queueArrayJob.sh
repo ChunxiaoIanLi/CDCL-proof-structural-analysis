d0e7b3d34b1faa75d43f7ce0853b06a76da9a25b
Jonathan Chung <jt2chung@uwaterloo.ca>
2021-02-12 10:51:19 -0500

    only run SAT solver jobs on graham nodes 1-800

--- scripts/resolvability/queueArrayJob.sh
+++ scripts/resolvability/queueArrayJob.sh
@@ -9,21 +9,28 @@ INPUT_FILE=$1
 START_INDEX=$2
 END_INDEX=$((START_INDEX + $3 - 1))
 SCRIPT_FILE="arrayJobScript.sh"
-SCRIPT="python /home/jt2chung/sha1-unsat/CDCL-proof-structural-analysis/hierarchical_community_structure/clustering_ed.py \${INSTANCE_NAME}"
+
+# Compute hierarchical parameters
+#SCRIPT="python /home/jt2chung/sha1-unsat/CDCL-proof-structural-analysis/hierarchical_community_structure/clustering_ed.py \${INSTANCE_NAME}"
+
+# Compute degree vector
 #SCRIPT="/home/jt2chung/sha1-unsat/CDCL-proof-structural-analysis/scripts/resolvability/countResolvable -d \${INSTANCE_NAME}"
-#SCRIPT="/home/jt2chung/sha1-unsat/maplesat_static -cpu-lim=4950 \"\${INSTANCE_NAME}\" > \"\${INSTANCE_NAME}.log\""
+
+# Run SAT solver
+SCRIPT="/home/jt2chung/sha1-unsat/maplesat_static -cpu-lim=4950 \"\${INSTANCE_NAME}\" > \"\${INSTANCE_NAME}.log\""
 
 # Generate script for sbatch
-echo "#!/bin/bash"                                                         > "${SCRIPT_FILE}"
-echo "#SBATCH --account=def-vganesh"                                      >> "${SCRIPT_FILE}"
-echo "#SBATCH --time=0:00:5000"                                           >> "${SCRIPT_FILE}"
-echo "#SBATCH --mem=10G"                                                   >> "${SCRIPT_FILE}"
-echo "#SBATCH --job-name=countRes"                                        >> "${SCRIPT_FILE}"
-echo "#SBATCH --array=${START_INDEX}-${END_INDEX}"                        >> "${SCRIPT_FILE}"
-echo ""                                                                   >> "${SCRIPT_FILE}"
-echo "source /home/jt2chung/sat/bin/activate"                             >> "${SCRIPT_FILE}"
-echo "INSTANCE_NAME=\`sed -n \${SLURM_ARRAY_TASK_ID}p ${INPUT_FILE}\`"    >> "${SCRIPT_FILE}"
-echo "${SCRIPT}"                                                          >> "${SCRIPT_FILE}"
+echo "#!/bin/bash"                                                              > "${SCRIPT_FILE}"
+echo "#SBATCH --account=def-vganesh"                                           >> "${SCRIPT_FILE}"
+echo "#SBATCH --time=0:00:5000"                                                >> "${SCRIPT_FILE}"
+echo "#SBATCH --mem=10G"                                                       >> "${SCRIPT_FILE}"
+echo "#SBATCH --array=${START_INDEX}-${END_INDEX}"                             >> "${SCRIPT_FILE}"
+echo "#SBATCH --exclude=gra[801-1325]"                                         >> "${SCRIPT_FILE}"
+echo ""                                                                        >> "${SCRIPT_FILE}"
+echo "echo \"Using node \${SLURMD_NODENAME} for job \${SLURM_ARRAY_TASK_ID}\"" >> "${SCRIPT_FILE}"
+echo "source /home/jt2chung/sat/bin/activate"                                  >> "${SCRIPT_FILE}"
+echo "INSTANCE_NAME=\`sed -n \${SLURM_ARRAY_TASK_ID}p ${INPUT_FILE}\`"         >> "${SCRIPT_FILE}"
+echo "${SCRIPT}"                                                               >> "${SCRIPT_FILE}"
 
 # Queue script
 sbatch ${SCRIPT_FILE}
